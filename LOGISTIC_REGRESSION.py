d09bbbb845ae8a7b01bf12244ab9f249c86b0122
Arun Kumar <99749067+chmson@users.noreply.github.com>
2023-05-20 19:12:17 +0530

    Add files via upload

---
+++ LOGISTIC_REGRESSION.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from matplotlib.pyplot import plot
+# %matplotlib inline
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+
+
+data = pd.read_csv("advertising.csv")
+
+data.info()
+data.describe()
+
+# Exploratory Data Analysis
+
+plt.figure(figsize=(8, 6))
+data.Age.hist(bins=data.Age.nunique())
+plt.xlabel('Age')
+
+plt.figure(figsize=(8, 6))
+sns.jointplot(x=data["Area Income"], y=data.Age)
+
+plt.figure(figsize=(8, 6))
+sns.jointplot(x=data["Daily Time Spent on Site"], y=data.Age, kind='kde')
+
+plt.figure(figsize=(8, 6))
+sns.jointplot(x=data["Daily Time Spent on Site"], y=data["Daily Internet Usage"])
+
+sns.pairplot(data, hue='Clicked on Ad')
+data['Clicked on Ad'].value_counts()
+plt.figure(figsize=(10, 7))
+sns.heatmap(data.corr(), annot=True)
+
+x = np.linspace(-6, 6, num=1000)
+plt.figure(figsize=(10, 6))
+plt.plot(x, (1 / (1 + np.exp(-x))))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Sigmoid Function")
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
+def print_score(clf, X_train, y_train, X_test, y_test, train=True):
+    if train:
+        pred = clf.predict(X_train)
+        clf_report = pd.DataFrame(classification_report(y_train, pred, output_dict=True))
+        print("Train Result:\n================================================")
+        print(f"Accuracy Score: {accuracy_score(y_train, pred) * 100:.2f}%")
+        print("_______________________________________________")
+        print(f"CLASSIFICATION REPORT:\n{clf_report}")
+        print("_______________________________________________")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_train, pred)}\n")
+        
+    elif train==False:
+        pred = clf.predict(X_test)
+        clf_report = pd.DataFrame(classification_report(y_test, pred, output_dict=True))
+        print("Test Result:\n================================================")        
+        print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
+        print("_______________________________________________")
+        print(f"CLASSIFICATION REPORT:\n{clf_report}")
+        print("_______________________________________________")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_test, pred)}\n")
+
+        from sklearn.preprocessing import StandardScaler, MinMaxScaler, OrdinalEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import train_test_split
+
+
+X = data.drop(['Timestamp', 'Clicked on Ad', 'Ad Topic Line', 'Country', 'City'], axis=1)
+y = data['Clicked on Ad']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# cat_columns = []
+num_columns = ['Daily Time Spent on Site', 'Age', 'Area Income', 'Daily Internet Usage', 'Male']
+
+
+ct = make_column_transformer((MinMaxScaler(), num_columns),(StandardScaler(), num_columns),remainder='passthrough')
+
+X_train = ct.fit_transform(X_train)
+X_test = ct.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+
+
+lr_clf = LogisticRegression(solver='liblinear')
+lr_clf.fit(X_train, y_train)
+
+print_score(lr_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(lr_clf, X_train, y_train, X_test, y_test, train=False)
